fc79ef993eab913e847e3f569362c3e5fbb0ce25
Jeremy Kim <jedo@alumni.unc.edu>
2025-05-01 22:54:12 -0400

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Jeremy Kim on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pick-a-PalTests/Pick_a_PalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalTests.swift
+//  Pick-a-PalTests
+//
+//  Created by Jeremy Kim on 5/1/25.
+//
+
+import Testing
+@testable import Pick_a_Pal
+
+struct Pick_a_PalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
