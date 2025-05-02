0ed5ddd04e0f630c53898c0f3d97b82db0076569
Jeremy Kim <jedo@alumni.unc.edu>
2025-05-01 23:19:11 -0400

    dynamic layout

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var names: [String] = ["Elisha", "Andre", "Jason", "Po-Chun"]
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(pickedName.isEmpty ? " " : pickedName)
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+            }
+            
+            TextField("Add Name", text: $nameToAdd)
+                .autocorrectionDisabled(true)
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Button("Pick Random Name") {
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                } else {
+                    pickedName = ""
+                }
+            }
         }
         .padding()
     }
